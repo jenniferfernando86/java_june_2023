4d9e0c4efe211b64e9f5a982d12734d4f885a9eb
jenniferfernando86 <jennifer.fernando86@gmail.com>
2023-07-15 02:16:12 -0400

    changed file

--- src/july11/AssignmentGreatestNumber.java
+++ src/july11/AssignmentGreatestNumber.java
@@ -1,6 +1,5 @@
 package july11;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class AssignmentGreatestNumber {
@@ -10,30 +9,57 @@ public class AssignmentGreatestNumber {
         Scanner scanner = new Scanner(System.in);
         int input;
         do{
-            System.out.println("Enter your first number");
+            System.out.print("Enter your first number : ");
             int first_number = scanner.nextInt();
-            System.out.println("Enter your second number");
+            System.out.print("Enter your second number : ");
             int second_number = scanner.nextInt();
-            System.out.println("Enter your third number");
+            System.out.print("Enter your third number : ");
             int third_number = scanner.nextInt();
 
             if (first_number > second_number && first_number > third_number)
             {
                 System.out.println( first_number +  " is greater than " + second_number + " & " + third_number);
             }else if (second_number > third_number) {
-                System.out.println(second_number + "is greater than " + first_number + " &" + third_number);
+                System.out.println(second_number + " is greater than " + first_number + " & " + third_number);
             }
             else{
-                System.out.println(third_number + "is greater than " + first_number + " &" + second_number);
+                System.out.println(third_number + " is greater than " + first_number + " & " + second_number);
             }
 
             System.out.println("Are you ready to exit");
-            System.out.println("Press 1 to continue or any other number to exit");
+            System.out.print("Press 1 to continue or any other number to exit : ");
             input = scanner.nextInt();
 
         }while(input == 1);
         System.out.println("Thankyou for using this program");
 
+        //using for loop
+        System.out.println("\nUsing For Loop");
+        for(int i = 0; i<1; i++) {
+            System.out.print("Enter your first number : ");
+            int first_number = scanner.nextInt();
+            System.out.print("Enter your second number : ");
+            int second_number = scanner.nextInt();
+            System.out.print("Enter your third number : ");
+            int third_number = scanner.nextInt();
 
+            if (first_number > second_number && first_number > third_number)
+            {
+                System.out.println( first_number +  " is greater than " + second_number + " & " + third_number);
+            }else if (second_number > third_number) {
+                System.out.println(second_number + " is greater than " + first_number + " & " + third_number);
+            }
+            else{
+                System.out.println(third_number + " is greater than " + first_number + " & " + second_number);
+            }
+            System.out.println("Are you ready to exit");
+            System.out.print("Press 1 to continue or any other number to exit : ");
+            input = scanner.nextInt();
+            if (input == 1) {
+                i = -1;
+            }
+
+        }
+        System.out.println("Thankyou for using this program");
     }
 }
